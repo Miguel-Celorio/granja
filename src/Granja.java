324373603052b439ff154b544fee1a5927186e8b
Miguel-Celorio <miguelcs60@educastur.es>
2021-11-19 13:05:18 +0100

    Commit inicial del proyecto

---
+++ src/Granja.java
@@ -0,0 +1,35 @@
+
+
+public class Granja
+{
+    private Granjero granjero;
+    private String nombre;
+    private String ubicacion;
+    
+    public Granja(Granjero granjero)
+    {
+        this.granjero = granjero;
+    }
+    
+    public Granja(String nombre, String ubicacion, Granjero granjero){
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.granjero = granjero;        
+    }
+    
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("Datos de la Granja: ");
+        sb.append("\n=====================");
+        sb.append("\nNombre: ");
+        sb.append(nombre);
+        sb.append("\nUbicacion: ");
+        sb.append(ubicacion);
+        sb.append("\nGranjero: ");
+        sb.append(granjero);
+        
+        return sb.toString();
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main
+{
+
+	public static void main(String[] args) {
+        Granjero paco = new Granjero("Paco", "Martinez", 58, 'H');
+        Granja granjaDePaco = new Granja("Pacolandia", "Corea del norte", paco);
+        Mascota mascota = new Mascota("Rex", 13, "Perro");
+        
+        paco.setMascota(mascota);
+        
+        System.out.println(granjaDePaco);
+    }
+    
+    
+    
+}
