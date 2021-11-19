6bb05a0a63121868bc72b86ff6c3162df95d4bdd
DARKSATAN <QIS64129@educastur.es>
2021-11-19 13:30:16 +0100

    escribi movidas

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main
 {
 
 	public static void main(String[] args) {
-        Granjero paco = new Granjero("Paco", "Martinez", 58, 'H');
-        Granja granjaDePaco = new Granja("Pacolandia", "Corea del norte", paco);
+        Granjero paco = new Granjero("Paco", "Paquez", 58, 'H');
+        Granja granjaDePaco = new Granja("Pacota del Sur", "Corea del norte", paco);
         Mascota mascota = new Mascota("Rex", 13, "Perro");
         
         paco.setMascota(mascota);
