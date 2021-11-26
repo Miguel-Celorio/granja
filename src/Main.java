ed74dd29ceba093fe07f06d45e801f3738e82508
Raúl <raulgr74@educastur.es>
2021-11-26 13:33:51 +0100

    quitado unas lineas de la clase main

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,6 @@ public class Main
         Granjero paco = new Granjero("Pacomaniaco del Pacodemonio", "Hijo de Paco", 58, 'H');
         Granja granjaDePaco = new Granja("Pacota del Sur", "Panama", paco);
         Mascota mascota = new Mascota("Rex", 13, "Perro");
-        
-        paco.setMascota(mascota);
-        
-        System.out.println(granjaDePaco);
     }
     
 }
